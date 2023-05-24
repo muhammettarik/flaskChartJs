911b648dc51444ae5d626123a1a31d33a382328f
muhammettarik <takiko337@gmail.com>
2023-05-24 17:36:43 +0300

    ajax

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from flask_socketio import SocketIO
 
 from random import randint
@@ -11,6 +11,20 @@ socketio = SocketIO(app)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# I am not making shared functions for getting different data to see different results
+@app.route('/data')
+def get_data():
+    dataSet = []
+
+    for i in range(5):
+        dataSet.append(str(randint(0,10)))
+
+    data = {
+        'data' : dataSet,
+            }
+
+    return jsonify(data)
 
 def update_data():
     while True:
